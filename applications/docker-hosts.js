82e05083fca85003bef9f962c6c18521dfef297e
Donovan Daniels <hewwo@yiff.rocks>
2023-06-19 18:49:14 -0500

    add duplicate tolerance

--- applications/docker-hosts.js
+++ applications/docker-hosts.js
@@ -1,102 +1,133 @@
-import { access, constants, copyFile, readFile, writeFile } from "fs/promises";
 import DockerEvents from "../dist/index.js";
 import { get } from "../dist/docker.js";
+import {
+    access,
+    constants,
+    copyFile,
+    readFile,
+    writeFile
+} from "node:fs/promises";
 
 const hostsFile = process.env.HOSTS_FILE ?? "/data/hosts";
 
 try {
     await access(hostsFile, constants.R_OK | constants.W_OK);
-} catch(err) {
+} catch (err) {
     throw new Error(`Hosts file "${hostsFile}" is either missing, or not readable/writable.`, { cause: err });
 }
 
 class Queue {
+    /** @type {Function[]} */
     static queue = [];
     static running = false;
 
+    /** @param {Function} fn */
     static add(fn, priority = false) {
-        if(priority === true) {
+        if (priority === true) {
             this.queue.unshift(fn);
         } else {
             this.queue.push(fn);
         }
 
-        if(this.running === false) {
+        if (this.running === false) {
             this.running = true;
-            this.next();
+            void this.next();
         }
+    }
+
+    static flush() {
+        this.queue = [];
+        this.running = false;
     }
 
     static async next() {
         const fn = this.queue.shift();
-        if(fn === undefined) {
+        if (fn === undefined) {
             this.running = false;
             return;
         }
         try {
             await fn();
-        } catch(err) {
-            console.error("Queue Error:", err);
+        } catch (err) {
+            if (cleanup === false) {
+                log("error", "Queue Error:", err);
+            }
         }
-        
-        this.next();
+
+        void this.next();
     }
 }
 
+let cleanup = false;
 const events = new DockerEvents({ socketPath: "/var/run/docker.sock" });
 process
-    .once("SIGINT", clean.bind(null, "SIGINT"))
-    .once("SIGTERM", clean.bind(null, "SIGTERM"));
-function clean(signal) {
-     events.close();
-    process.kill(process.pid, signal);
+    .on("SIGINT", clean)
+    .on("SIGTERM", clean);
+function clean() {
+    cleanup = true;
+    Queue.flush();
+    events.close();
+    process.kill(process.pid, "SIGKILL");
 }
 
-console.log("Launching - refreshing");
+log("log", "Launching - refreshing");
 await refresh();
 
 events
-    .on("container.start", async(data) => {
-        if(data.Actor.Attributes.hostname !== undefined) {
+    .on("container.start", async data => {
+        if (data.Actor.Attributes.hostname !== undefined) {
             Queue.add((async() => {
-                console.log("Got start for %s - refreshing", data.Actor.Attributes.hostname);
+                log("group", "Got start for %s - refreshing", data.Actor.Attributes.hostname);
                 await refresh();
+                console.groupEnd();
             }));
         }
     })
-    .on("container.stop", async(data) => {
-        if(data.Actor.Attributes.hostname !== undefined) {
+    .on("container.stop", async data => {
+        if (data.Actor.Attributes.hostname !== undefined) {
             Queue.add((async() => {
-                console.log("Got stop for %s - refreshing", data.Actor.Attributes.hostname);
+                log("group", "Got stop for %s - refreshing", data.Actor.Attributes.hostname);
                 await refresh();
+                console.groupEnd();
             }));
         }
     })
-    .on("container.die", async(data) => {
-        if(data.Actor.Attributes.hostname !== undefined) {
+    .on("container.die", async data => {
+        if (data.Actor.Attributes.hostname !== undefined) {
             Queue.add((async() => {
-                console.log("Got die for %s - refreshing", data.Actor.Attributes.hostname);
+                log("group", "Got die for %s - refreshing", data.Actor.Attributes.hostname);
                 await refresh();
+                console.groupEnd();
             }));
         }
     })
-    .on("error", (err) => {
-        console.error("Events Error:", err);
-    })
+    .on("error", err => {
+        log("error", "Events Error:", err);
+    });
+
+/**
+ *
+ * @param {("log" | "warn" | "debug" | "error" | "group")} type
+ * @param {string} formatter
+ * @param  {...unknown} args
+ */
+function log(type, formatter, ...args) {
+    console[type](`\u001B[90m%s \u001B[0m${formatter}`, new Date().toISOString(), ...args);
+}
 
 async function refresh() {
-    const content = (await readFile(hostsFile, { encoding: "utf-8" })).split("\n");
+    const content = (await readFile(hostsFile, { encoding: "utf8" })).split("\n");
     const startIndex = content.findIndex(d => d.startsWith("# begin docker-hosts"));
     const endIndex = content.findIndex(d => d.startsWith("# end docker-hosts"));
-    if(startIndex === -1) {
-        console.debug("No start index found, assuming first run");
-        if(endIndex !== -1) {
-            console.warn("Found end index, but no start index. The end index will be removed, and a new section will be created at the end of the file.");
+    if (startIndex === -1) {
+        log("debug", "[Refresh] No start index found, assuming first run");
+        if (endIndex !== -1) {
+            log("warn", "[Refresh] Found end index, but no start index. The end index will be removed, and a new section will be created at the end of the file.");
             content.splice(endIndex, 1);
         }
     } else {
-        if(endIndex === -1) {
-            console.warn("Found start index, but no end index. The start index will be removed, and a new section will be created at the end of the file.");
+        if (endIndex === -1) {
+            log("warn", "[Refresh] Found start index, but no end index. The start index will be removed, and a new section will be created at the end of the file.");
             content.splice(startIndex, 1);
         } else {
             // console.debug("Found start and end index, removing all lines between them.");
@@ -105,18 +136,76 @@ async function refresh() {
     }
 
 
-    const hosts = await getHosts();
+    const containers = await getHosts();
+    /** @type {import("./types").HostDuplicate[]} */
+    const duplicateHosts = [];
+    /** @type {string[]} */
+    const seen = [],
+        /** @type {string[]} */
+        duplicate = [];
+    for (const container of containers) {
+        if (duplicate.includes(container.host)) {
+            continue;
+        }
+
+        if (seen.includes(container.host)) {
+            duplicate.push(container.host);
+        } else {
+            seen.push(container.host);
+        }
+    }
+
+    for (const host of duplicate) {
+        duplicateHosts.push({ host, containers: containers.filter(c => c.host === host) });
+    }
+
+    for (const d of duplicateHosts) {
+        console.group(`[Refresh] Duplicate host ${d.host} found on ${d.containers.length} containers`);
+        const hasNumbers = d.containers.every(c => /-\d+$/.test(c.name));
+        /** @type {import("./types").ContainerInfo | undefined} */
+        let container;
+        if (hasNumbers) {
+            log("debug", "Containers seem to be numbered, using highest container.");
+            const highest = d.containers.reduce((prev, curr) => {
+                const num = Number(curr.name.at(-1));
+                return isNaN(num) ?  prev : Math.max(prev, num);
+            }, -1);
+            container = d.containers.find(c => c.name.endsWith(`-${highest}`));
+            if (container === undefined) {
+                log("error", "Failed to find highest numbered container, using first container.");
+                container = d.containers[0];
+            }
+        } else {
+            log("warn", "Containers do not seem to be numbered, using first container.");
+            container = d.containers[0];
+        }
+
+        if (container === undefined) {
+            log("error", "Failed to pick container to assign host.");
+        }
+
+        for (const c of d.containers) {
+            if (c === container) {
+                continue;
+            }
+            log("debug", "Removing container %s (%s).", c.container, c.name);
+            const index = containers.indexOf(container);
+            containers.splice(index, 1);
+        }
+        console.groupEnd();
+    }
+
     let maxIPLen = 0, maxHostLen = 0, maxNameLen = 0, maxContainerLen = 0;
-    for(const { container, host, ip, name } of hosts) {
+    for (const { container, host, ip, name } of containers) {
         maxIPLen = Math.max(maxIPLen, ip.length);
         maxHostLen = Math.max(maxHostLen, host.length);
         maxNameLen = Math.max(maxNameLen, name.length);
         maxContainerLen = Math.max(maxContainerLen, container.length);
     }
-    const newContent = ["# begin docker-hosts", ...hosts.map(d => `${d.ip.padEnd(maxIPLen, " ")} ${d.host.padEnd(maxHostLen, " ")} # ${d.name.padEnd(maxNameLen, " ")} (${d.container.padEnd(maxContainerLen, " ")})`), "# end docker-hosts"];
+    const newContent = ["# begin docker-hosts", ...containers.map(d => `${d.ip.padEnd(maxIPLen, " ")} ${d.host.padEnd(maxHostLen, " ")} # ${d.name.padEnd(maxNameLen, " ")} (${d.container.padEnd(maxContainerLen, " ")})`), "# end docker-hosts"];
     content.push(...newContent);
     await copyFile(hostsFile, `${hostsFile}.bak`);
-    await writeFile(hostsFile, content.join("\n"), { encoding: "utf-8" });
+    await writeFile(hostsFile, content.join("\n"), { encoding: "utf8" });
 }
 
 async function getHosts() {
--- applications/types.d.ts
+++ applications/types.d.ts
@@ -1,13 +1,25 @@
 export interface Container {
-    Id: string;
-    Names: string[];
-    Labels: Record<string, string>;
     HostConfig: {
         NetworkMode: string;
     };
+    Id: string;
+    Labels: Record<string, string>;
+    Names: Array<string>;
     NetworkSettings: {
         Networks: Record<string, {
             IPAddress: string;
         }>;
     };
+}
+
+export interface HostDuplicate {
+    containers: Array<ContainerInfo>;
+    host: string;
+}
+
+export interface ContainerInfo {
+    container: string;
+    host: string;
+    ip: string;
+    name: string;
 }
